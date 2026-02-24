ca79341b2c62c2f56fa511f7ad3690957a1c8704
YixiZ04 <leonzhangcoc@gmail.com>
2026-02-24 17:27:41 +0100

    Add files

--- RepoRT_trials/1.RepoRT_GNNs0/1.RepoRT_GNNs0.py
+++ RepoRT_trials/1.RepoRT_GNNs0/1.RepoRT_GNNs0.py
@@ -6,6 +6,7 @@ Basic MPNN is built here, this is, using all default parameters of chemprop.
     * ffn_n_layers = 1
     * ffn_hidden_dim = 300
     * final_lr = 1e-4
+The train_val loss plot is not included for each repo data since EarlyStopping mechanism is implemented. Also, no checkpoints or model file are saved.
 """
 
 # 0. Import modules
@@ -15,33 +16,34 @@ import numpy as np
 from chemprop import data, nn, models, featurizers
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 from lightning import pytorch as pl
+import os
 
-# 1. Def training function
-def get_data_from_index (index, df):
-    temp_dataframe = df[df ["id"].str.split("_").str[0] == index]
-    return temp_dataframe
+# 1. Define the functions we are gonna need
 def get_index_array (num_repos):
+    """
+    Input: The number of repos you want to use for training
+    Output: a numpy array containing the indexes string in the specific format ("0001", e.g.)
+    """
     index_array = []
-    for index in range(num_repos):
+    for index in range(1, num_repos+1):
         index = str (index)
         while len (index) < 4:
             index = "0" + index
+        index_array.append (str (index))
     return np.array(index_array)
 
-def get_data_from_each_repo (df, index):
-    failed_model = 0
-    temp_dataframe = get_data_from_index(index, df)
-    if len (temp_dataframe) == 0:
-        failed_model += 1
-        print (f"The repo nº {index} is has no data.")
-    else:
-        print (f"Using data from repo {index} to train a chemprop model...")
-        temp_dataframe = get_data_from_index(index, df)
+def get_data_from_index (df, index):
+    """
+    Input: The raw dataframe containing all data and the index of the repo that you want to use for training.
+    Output: A subset of the dataframe including all the molecules from the repo number.
+    """
+    temp_dataframe = df[df ["id"].str.split("_").str[0] == index]
     return temp_dataframe
+
 def get_dataloader_from_df (df):
     """
-    Train a chemprop model using data given from a pandas dataframe.
-    As here we are only trying default values, no parameters are given as input to this function.
+    Input: a pandas dataframe containing the information for training.
+    Outputs: Scaler used for scaling the targets. Train, val and test dataloaders built from the the dataframe. An array contaning the test indices.
     """
     smiles = df.loc [:, "smiles"].values
     rts = df.loc [:, ["rt_s"]].values
@@ -53,9 +55,6 @@ def get_dataloader_from_df (df):
 
     mols = [ d.mol for d in all_data]
     train_indices, val_indices, test_indices = data.make_split_indices(mols, "random", (0.8, 0.1, 0.1))
-    train_data, val_data, test_data = data.split_data_by_indices(
-        all_data, train_indices, val_indices, test_indices
-    )
     train_data, val_data, test_data = data.split_data_by_indices(
         all_data, train_indices, val_indices, test_indices
     )
@@ -75,7 +74,6 @@ def get_dataloader_from_df (df):
     train_loader = data.build_dataloader(train_dset, num_workers=5)
     val_loader = data.build_dataloader(val_dset, num_workers=5, shuffle=False)
     test_loader = data.build_dataloader(test_dset, num_workers=5, shuffle=False)
-    print ("The dataloaders successfully built...")
     return scaler,train_loader, val_loader, test_loader, test_indices
 
 def configure_train_model (train_loader, val_loader, test_loader,scaler, mp_hidden_dim=300, ffn_layers=1, ffn_hidden_dim=300,final_lr=1e-4):
@@ -88,16 +86,17 @@ def configure_train_model (train_loader, val_loader, test_loader,scaler, mp_hidd
     batch_norm = True
     output_transform = nn.UnscaleTransform.from_standard_scaler(scaler)
     ffn = nn.RegressionFFN (
-        input_size=mp_hidden_dim,
+        input_dim=mp_hidden_dim,
         hidden_dim = ffn_hidden_dim,
         n_layers = ffn_layers,
-        output_dim = output_transform,
+        output_transform = output_transform,
         criterion=nn.MSE(),
     )
     mpnn = models.MPNN (mp, agg, ffn, batch_norm, final_lr=final_lr)
     es_cb = EarlyStopping (patience=10, monitor="val_loss", mode="min")
     trainer = pl.Trainer (
         accelerator = "auto",
+        devices = 1,
         max_epochs = 1000,
         callbacks = [es_cb],
     )
@@ -109,56 +108,89 @@ def configure_train_model (train_loader, val_loader, test_loader,scaler, mp_hidd
     return res
 
 def save_res_table (df, test_indices, pred_array, save_dir):
-    id_array = df ["id"]
-    smiles = df ["smiles"]
-    real_rts = df ["rt_s"]
+    """
+    Inputs: A pandas dataframe containing the data used for training. An array (2D) containing the test indices. An array containing the results from prediction.
+    A filename (Absolute path + filename, e.g.). to save the .tsv file.
+    Output: A pandas dataframe containing the results and the differences (Not sorted) and a saved .tsv file in the indicated dir path.
+    """
+    id_array = df.loc [:,"id"].values
+    smiles = df.loc [:,"smiles"].values
+    real_rts = df.loc [:,"rt_s"].values
     test_ids = []
     test_smiles = []
     test_rts = []
-    for index in test_indices:
+    pred_res = []
+    for index in test_indices [0]:
         id = id_array[index]
         smile = smiles[index]
         real_rt = real_rts[index]
         test_ids.append(id)
         test_smiles.append(smile)
         test_rts.append (real_rt)
+    for res in pred_array:
+        pred_res.append(round(res[0],2))
     result_table = pd.DataFrame ({
         "id": test_ids,
         "smile": test_smiles,
         "real_rt": test_rts,
-        "pred_rt": pred_array,
+        "pred_rt": pred_res,
+        "diff": [round (np.abs (ps - ts),2) for ps,ts in zip(pred_res, test_rts)],
+        "diff_sq": [round ((ps - ts)**2, 2) for ps,ts in zip(pred_res, test_rts)]
     })
     result_table.to_csv (save_dir, sep='\t', index=False)
     return result_table
 
-def mae_rmse_form_datatable (res_table):
+def save_mae_rmse_form_datatable (res_table, save_file):
     """
-        Input: DataFrame with target and predicted retention times.
-        Output: MAE and RMSE calculated from those values.
-        """
-    sum_num_RMSE = 0
-    sum_num_MAE = 0
-    m = len(res_table["real_rt"])
-    for i in range(m):
-        diff = (res_table["real_rt"][i] - res_table["pred_rt"][i])
-        diff_MAE = np.abs(diff)
-        diff_RMSE = diff ** 2
-        sum_num_RMSE += diff_RMSE
-        sum_num_MAE += diff_MAE
-    sum_num_RMSE = sum_num_RMSE / m
-    MAE = (sum_num_MAE / m)
-    RMSE = (np.sqrt(sum_num_RMSE))
-    return (MAE, RMSE)
+    Input: DataFrame with target and predicted retention times.
+    Output: MAE and RMSE calculated from those values.
+    """
+    diffs = res_table ["diff"]
+    diff_sqs = res_table ["diff_sq"]
+    n = len (diffs)
+    MAE = np.sum (diffs) / n
+    RMSE = np.sqrt (np.sum (diff_sqs) / n)
+    return MAE, RMSE
 
-def get_results (df, )
-
-
-
-
-
-
+def train_model_for_every_repo (df, save_dir, num_repos2train=450):
+    """
+    Inputs: A pandas dataframe containing all data. Seed name for the save dir ("/home/user/.../Results_). The num of the repos to use for training.
+    Output: Saved results files in the repo for each index.
+    Note: If a repo contains less than 20 molecules (empty repos included), will not be used for training as errors occur during training.
+    """
+    indexes = get_index_array(num_repos2train)
+    for index in indexes:
+        temp_df = get_data_from_index(df, index)
+        if len(temp_df) < 20:
+            print (f"The repo nº {index} is empty or not enough molecules are contained. It will be skipped")
+            continue
+        else:
+            print (f"Starting training for repo {index}...")
+            real_save_dir = save_dir +index+ "/"
+            os.makedirs (real_save_dir, exist_ok=True)
+            table_file = real_save_dir + "Results_" + index + ".tsv"
+            metric_file = real_save_dir + "Metrics_" + index + ".txt"
+            scaler, train_loader, val_loader, test_loader, test_indices = get_dataloader_from_df(temp_df)
+            print (f"Successfully built the dataloaders for nº repo {index}...")
+            temp_pred_res = configure_train_model(train_loader, val_loader, test_loader, scaler)
+            res_table = save_res_table(temp_df, test_indices, temp_pred_res, table_file)
+            MAE, RMSE = save_mae_rmse_form_datatable(res_table, metric_file)
+            with open(metric_file, "w") as f:
+                f.write(f"MAE: {MAE:.4f} s\nRMSE: {RMSE:.4f} s")
+            print (f"The MPNN for the repo {index} has been trained successfully and the results are saved in {real_save_dir}.")
+    print ("All models are successfully trained and the results are saved!")
 
 
 # 2. Load Data
 
-data_table = pd.read_csv ("C:/Users/leonz/Github_reps/TFG_git/RepoRT_trials/RepoRT_data/RepoRT_data_clean.tsv", sep="\t")
+path2data = "/home/user/YixiTFG/TFG_Yixi/RepoRT_trials/RepoRT_data/RepoRT_data_clean.tsv"
+df = pd.read_csv (path2data, sep='\t')
+save_dir = "/home/user/YixiTFG/TFG_Yixi/RepoRT_trials/1.RepoRT_GNNs0/Results/Results_"
+train_model_for_every_repo(df, save_dir, num_repos2train=450)
+
+
+#temp_data = get_data_from_index(df, "0022")
+#len (temp_data)
+#scaler, train_loader, val_loader, test_loader, test_indices = get_dataloader_from_df(temp_data)
+#temp_pred_res = configure_train_model(train_loader, val_loader, test_loader, scaler)
+#res_table = save_res_table(temp_data, test_indices, temp_pred_res, save_dir)
---
+++ RepoRT_trials/RepoRT_mol_desc_data/get_mol_desc_RepoRT.py
@@ -0,0 +1,41 @@
+"""
+"""
+
+# 0. Import modules
+
+import pubchempy as pcp
+import pandas as pd
+import numpy as np
+
+# 1. Define a function to get mol_desc
+
+def get_mol_desc_from_inchi (inchi_array):
+    """ 
+    Input: an inchi array.
+    Output: molecular weight and xlogp arrays
+    """
+    mol_weight_array = []
+    xlop_array = []
+    for inchi in inchi_array:
+        try:
+            print (f"Fetching mol desc for the molecule: {inchi}:")
+            molecule = pcp.get_compounds(str(inchi), namespace="inchi")
+            mol_weight = np.float16(molecule[0].molecular_weight)
+            xlogp = np.float16 (molecule[0].xlogp)
+            mol_weight_array.append(mol_weight)
+            xlop_array.append(xlogp)
+        except:
+            print(f"Cannot fetch mol desc for the molecule: {inchi}.")
+            mol_weight_array.append(np.nan)
+            xlop_array.append(np.nan)
+
+    return mol_weight_array, xlop_array
+
+# 2. Load the data
+
+path2file = "/home/user/YixiTFG/TFG_Yixi/RepoRT_trials/RepoRT_data/RepoRT_data_clean.tsv"
+df = pd.read_csv(path2file, sep="\t")
+inchi_array = df.loc [:,"inchi"].values
+
+mol_weight_array, xlop_array = get_mol_desc_from_inchi (inchi_array)
+
