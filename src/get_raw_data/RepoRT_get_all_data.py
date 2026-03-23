fixed concept errors and updated the scaling

src/get_raw_data/RepoRT_get_all_data.py
@@ -2,15 +2,16 @@
 Name: RepoRT_get_all_data.py
 Author: Yixi Zhang
 Date: March 2026.
-Version: 1.0
+Version: 1.1.
 This will get the raw data from RepoRT and merge them into one single dataframe them export it to a tsv file.
 Some preprocessing to the data will be done:
-    1. All NA values would be filled with zero in the gradient data.
+    1. All NA values would be filled with zero in the gradient data, but the flowrate values.
     2. All the columns that have not gradient data included will be dropped.
     3. New column "id" will be added, this refers to the dataset's id, e.g., "0001, "0002", "0003"...
     4. The "formula" column will be updated with the real formula for the molecule usign RDkit.
     5. Add a column "rt_s" containing the retention time converted to seconds.
-This Script would need a version update, only if the format of RepoRT were changed, namely:
+    6. A pre-filter fot those repositories that gradient time data is not available.
+This Script would need a version update, only if the format of RepoRT were changed, namely: (I lied)
 the mol_data's url no longer being:https://raw.githubusercontent.com/michaelwitting/RepoRT/refs/heads/master/processed_data/{repo_index}/{repo_index}_rtdata_canonical_success.tsv
 the gradient_data's url no longer being: https://raw.githubusercontent.com/michaelwitting/RepoRT/refs/heads/master/processed_data/{repo_index}/{repo_index}_gradient.tsv
 or
@@ -19,8 +20,12 @@ or any format inside those files were changed,
 If any repo were add lately in RepoRT, only need to re-run the file changing the variable "num_repos".
 
 NOTE: Internet connexion is required and takes several minutes to build thw datafile from scratch
-"""
 
+Update: In the previous version, the doublets was not considered, meaning that if the isomeric SMILES exists for a repo, the canonical and the isomeric SMILES were both included in the dataset;
+so now in this new version, this is fixed by overwriting the canonical SMILES with the isomeric SMILES if possible.
+Also, the flowrate data has also been better treated, as in the previous version, the nan values in flow_rate data was not considered, so in the final datafile it is 0 in those repositories
+where flowrate = nan; in this version, this has been fixed by filling them with mean flowrate values.
+"""
 # Import modules
 
 import numpy as np
@@ -85,7 +90,8 @@ def get_mol_formula_by_smiles (smiles):
 def get_rt_data (num_repos=num_repos,seed_url=seed_url):
     """
     Input: num_repos to fetch, and the seed url for RepoRT.
-    Output: a table containing the RT data fetched from RepoRT
+    Output: a table containing the RT data fetched from RepoRT.
+    Update: fixed the concept error, as now the isomeric success will only be used for updating the cononical success data table.
     """
     final_dataframe = pd.DataFrame()
     index_array = get_index_array(num_repos)
@@ -96,13 +102,13 @@ def get_rt_data (num_repos=num_repos,seed_url=seed_url):
         try:
             temp_dataframe_can = pd.read_csv(can_url, sep="\t", encoding="utf-8")
             temp_dataframe_iso = pd.read_csv(iso_url, sep="\t", encoding = "utf-8")
-            if temp_dataframe_can.shape [0] != 0 and temp_dataframe_iso.shape [0] != 0:
-                final_dataframe = pd.concat ([final_dataframe, temp_dataframe_can, temp_dataframe_iso], ignore_index=True)
-            elif temp_dataframe_iso.shape [0] == 0 and temp_dataframe_can.shape [0] != 0:
-                final_dataframe = pd.concat ([final_dataframe, temp_dataframe_can], ignore_index=True)
-            else:
-                print (f"The repo {index} does not contain any data. It will be skipped.")
-        except:
+            # Use the id of the molecule as index followed by the updating.
+            temp_dataframe_can = temp_dataframe_can.set_index ("id")
+            temp_dataframe_iso = temp_dataframe_iso.set_index ("id")
+            temp_dataframe_can.update(temp_dataframe_iso) #Here, if isomiric success is not emply
+            temp_dataframe_can = temp_dataframe_can.reset_index ()
+            final_dataframe = pd.concat ([final_dataframe, temp_dataframe_can], ignore_index=True)
+        except: #The only error here is that the URL has not been found
             print (f"The repo nº {index} has not been found in the dataset. It will be skipped...")
     dir_id_array = [idmol.split("_")[0] for idmol in final_dataframe["id"]]
     dir_id_array = np.array(dir_id_array)
@@ -323,6 +329,30 @@ def get_gradient_data (num_repos = num_repos, seed_url = seed_url):
     del temp_df, temp_df_row_final, temp_df_row
     return final_df
 
+def update_flow_rate (df, max_grad_num = 18):
+    """
+    This function updates all flowrate in the gradient data. There are nan values in flowrates and in the fetching process, they were filled with 0.
+    So here, we update those values of 0 with the flowrate inferred using the mean flowrate of all the repositories.
+    This updating process is only applied to the segments of gradient where it exists (time of the segment !=0), meaning for example, if in a repo only 7 segments,
+    all the other segments from 8 to final will not be filled with flowrate.
+    """
+    updated_df = []
+    index_array = np.unique(df["dir_id"])
+    for index in index_array:
+        print (index)
+        temp_df = df[df["dir_id"] == index].reset_index(drop = True)
+        fr_array = temp_df.loc [:,"column.flowrate"].values
+        for grad_index in range(max_grad_num):
+            time_column_name = "t [min]_" + str(grad_index)
+            fr_column_name = "flow rate [ml/min]_" + str(grad_index)
+            time = temp_df.loc[0, time_column_name]
+            if time != 0 and grad_index > 0:
+                temp_df [fr_column_name] = fr_array
+            else:
+                continue
+        updated_df.append (temp_df)
+    return pd.concat (updated_df, ignore_index = True)
+
 def get_raw_datatable(save_dir = save_dir):
     """
     This function makes the raw datatable from RepoRT with all data processed and saves it in the save_dir.
@@ -360,6 +390,7 @@ def get_raw_datatable(save_dir = save_dir):
     print (f"Building the raw datatable and saving it in {save_file}...")
     temp_final_datatable = pd.merge (temp_column_metadata, temp_grad_data, on = "dir_id", how = "inner")
     final_datatable = pd.merge (temp_rt_data, temp_final_datatable, on = "dir_id", how = "inner")
+    final_datatable = update_flow_rate (final_datatable)
 
     # Export the final datatable
     final_datatable.to_csv (save_file, sep = "\t", index = False)
@@ -370,7 +401,6 @@ def get_raw_datatable(save_dir = save_dir):
 #Get raw datatable
 if __name__ == "__main__":
     get_raw_datatable()
-    sys.exit (0)
 
 
 

src/process_RepoRT_data/data_processing.py
@@ -2,26 +2,32 @@
 Name: data_processing.py
 Author: Yixi Zhang
 Date: March 2026
-Version: 1.1.
+Version: 1.2.
 Usage: The processing method:
-        1. Keep only those columns used for training.
-        2. Drop those molecules having > 10 gradients.
-        3. Normalize values that need normalization, e.g., the column.length, column.ta, column.fr... using StandardScaler.
+    1. Keep only those columns used for training.
+    2. Drop those molecules having > 10 gradients.
+    3. Get max and mean RT for each repository, merge them to each molecule according the dir_id.
 Only functions are defined here, but if called the function "get_processed_df_from_raw", the processed datafile will be created and saved in:
 ./data/processed_RepoRT/complete_treated_data.tsv.
 (See ./src/get_RepoRT_data/RepoRT_get_all_data.py)
-Update: Filter and downsampling mechanism are included:
-    1. The repositories containing less than 100 molecules will be removed from the final dataset (100 is the default value, it can be changed).
-    2. Those repositories containing more than 5000 molecules will be random sampled of 5000 molecules (Downsampling).
-    If ran with all default values, a new datafile will be built in ./data/processed_data/ named "filterd_treated_data.tsv".
-    If ran with "complete" Boolean set to True, the original datafile will be saved in ./data/processed_data/ named "complete_treated_data.tsv".
+Update: Rectified a concept error here, as the final result table should not be scaled, and the scaling process will be done after data splitting using train set Scaler
+Also, on top of the previous update, here more options are given:
+    1. Drop completely the SMRT dataset and apply down_threshold. (Drop datasets if contains less molecules than the threshold).
+    2. Not dropping completely the SMRT and apply a up_threshold, meaning that we are performing a downsampling process.
+Overall, if all options were tested, this file could produce 6 different tsv files in ./data/processed_RepoRT/ and identified by their filename:
+    1. no_SMRT_complete_data.tsv (Dropped SMRT but no any filter has been applied, this is, it contains repos < 100 molecules)
+    2. no_SMRT_ds_data.tsv (Dropped SMRT and applied downsampling (ds), the repos contains 100 < n < 5000 molecules)
+    3. no_SMRT_no_ds_data.tsv (Dropped SMRT and applied ds, the repos contains n > 100 molecules.
+    4. with_SMRT_complete_data.tsv (Not dropped SMRT and repos contains any number of molecules).
+    5. with_SMRT_ds_data.tsv. This would be the "filtered_treated_data.tsv" in the previous version applying the filters.
+    6. with_SMRT_no_ds_data.tsv. Containing SMRT and no ds applied, but all repos contains n > 100 molecules.
+NOTE: This is possible to create all 6 files, but it does not mean that we will evaluate models on all of them.
 """
 # IMPORT MODULES AND SCRIPTS
 import pandas as pd
 import numpy as np
 import os
 from pathlib import Path
-from sklearn.preprocessing import StandardScaler
 from src.get_raw_data.RepoRT_get_all_data import get_raw_datatable
 
 # DEFINE  FUNCTIONS
@@ -61,43 +67,6 @@ def update_df_drop_grad (df, threshold):
     updated_df = new_temp_df.iloc[:, :position]
     return updated_df
 
-
-def get_updated_meta_grad_data (df):
-    """
-    This is used for getting updated gradient and column metadata from a treated dataframe.
-    Get the first row from each index existing in df.
-    """
-    temp_list = []
-    index_array = np.unique (df["dir_id"])
-    for index in index_array:
-        temp_df = df[df["dir_id"] == index]
-        row = temp_df.iloc[:1]
-        temp_list.append (row)
-
-    return pd.concat(temp_list)
-
-def scaling_meta_grad_data (updated_df):
-    """
-    Requieres a df that only contains 1 metadata and gradient data per repo.
-    Returns the same dataframe but scaled.
-    """
-    temp_df = updated_df.loc[:, "column.length":].astype(float)
-    std_scaler = StandardScaler()
-    scaled_array = std_scaler.fit_transform(temp_df)
-    for column, array in zip (temp_df.columns, scaled_array.transpose()):
-        temp_df[column] = array
-    temp_df.insert(0, "dir_id", updated_df["dir_id"])
-    return temp_df
-
-def get_complete_scaled_df (df, updated_df):
-    """
-    Use the previous unscaled dataframe and the scaled metadata and gradient dataframe.
-    And merges them together to get the complete scaled dataframe.
-    """
-    temp_df = df.loc[:, :"column.usp.code_nan"]
-    final_df = pd.merge (temp_df, updated_df, on = "dir_id", how = "inner")
-    return final_df
-
 def get_max_mean_rt_per_cc (complete_df):
     """
     This get the max and mean rt for every chromatography column and inserts them next to "rt_s" column of the dataframe.
@@ -118,28 +87,46 @@ def get_max_mean_rt_per_cc (complete_df):
     position = complete_df.columns.get_loc ("rt_s")
     complete_df.insert (position + 1, "max_rt", max_array)
     complete_df.insert (position + 2, "mean_rt", mean_array)
-    return complete_df
 
-def filter_by_n_mol (df, down_threshold = 100, up_threshold = 5000):
+def filter_by_n_mol (df, down_threshold = 100, up_threshold = 5000, apply_upthreshold = False):
     """
-    Input: A threshold for filetering RepoRT data. Those repositories containing less than threshold number of molecule will be removed.
-    If the cc has more molecules than up_thereshold, a downsampling will be applied to that repo and retains the up_threshold number of molecules.
-    Output: The updataed dataframe containing the filtered data.
+    Input: A threshold for filtering RepoRT data. Those repositories containing less than threshold number of molecule will be removed.
+    If the cc has more molecules than up_threshold, a downsampling will be applied to that repo and retains the up_threshold number of molecules.
+    apply_upthreshold (False): Boolean to decide whether to perform downsampling or not.
+    Output: The updated dataframe containing the filtered data.
     """
     index_array = np.unique (df["dir_id"])
     final_df = []
-    for index in index_array :
-        temp_df = df[df["dir_id"] == index]
-        if down_threshold < temp_df.shape[0] < up_threshold:
-            final_df.append (temp_df)
-        elif temp_df.shape[0] > up_threshold:
-            final_df.append (temp_df.sample (up_threshold))
-        else:
-            continue
+    if apply_upthreshold:
+        for index in index_array :
+            temp_df = df[df["dir_id"] == index]
+            if down_threshold < temp_df.shape[0] < up_threshold:
+                final_df.append (temp_df)
+            elif temp_df.shape[0] > up_threshold:
+                final_df.append (temp_df.sample (up_threshold))
+            else:
+                continue
+    else:
+        for index in index_array:
+            temp_df = df[df["dir_id"] == index]
+            if down_threshold < temp_df.shape[0]:
+                final_df.append(temp_df)
+            else:
+                continue
     return pd.concat (final_df)
 
 
-def get_processed_df_from_raw (complete = False, input_path = "./data/no_extra_mol_desc/RepoRT_complete_data.tsv", path2res = "./data/processed_RepoRT/",grad_num2drop = 11, down_threshold = 100, up_threshold = 5000):
+
+def get_processed_df_from_raw (input_path = "./data/no_extra_mol_desc/RepoRT_complete_data.tsv",
+                               path2res = "./data/processed_RepoRT/",
+                               grad_num2drop = 11,
+                               down_threshold = 100,
+                               up_threshold = 5000,
+                               complete = False,
+                               drop_smrt = True,
+                               apply_upthreshold = False,
+                               smrt_id = 186,
+                               ):
     """
     Input: complete (Boolean), default set to False to get the filtered data; if set to true, the complete data without filtering is fetched.
     input_path to RepoRT raw data.
@@ -149,7 +136,10 @@ def get_processed_df_from_raw (complete = False, input_path = "./data/no_extra_m
     Output: Depending on how "complete" Boolean is set, the result file will be different:
         *False: ./data/processed_RepoRT/filtered_treated_data.tsv
         *True: ./data/processed_RepoRT/complete_treated_data.tsv
-
+    Update: Takes 2 new Booleans and has 1 default parameter:
+        *drop_smrt (True): True if want to drop completely the SMRT dataset.
+        *apply_upthreshold (False): True if want to perform downsampling.
+        *smrt_id (186): This is the dir_id of SMRT, only change if RepoRT reorganized the directory IDs, which is not very likely
     """
     print (f"Checking for input file...")
     file = Path (input_path)
@@ -159,6 +149,12 @@ def get_processed_df_from_raw (complete = False, input_path = "./data/no_extra_m
         print (f"Input file not found, creating new file...")
         get_raw_datatable()
     df = pd.read_csv(input_path, sep = "\t")
+    if drop_smrt: #Not only decides if drop SMRT at all, but also initialize the filename for saving.
+        print ("Dropping SMRT data...")
+        filename = path2res + "no_SMRT_"
+        df = df[df["dir_id"] != smrt_id]
+    else:
+        filename = path2res + "with_SMRT_"
 
     print ("Checking for saving directory...")
     os.makedirs(path2res, exist_ok = True)
@@ -167,21 +163,23 @@ def get_processed_df_from_raw (complete = False, input_path = "./data/no_extra_m
     print ("Starting processing the data...")
     temp_df = keep_only_useful_columns(df)
     temp_df = update_df_drop_grad(temp_df, grad_num2drop)
-    updated_meta_grad_data = get_updated_meta_grad_data(temp_df)
-    scaled_meta_grad_data = scaling_meta_grad_data(updated_meta_grad_data)
-    final_complete_df = get_complete_scaled_df(temp_df, scaled_meta_grad_data)
-    final_complete_df = get_max_mean_rt_per_cc(final_complete_df)
-    if complete == True:
-        filename = path2res + "complete_treated_data.tsv"
+    get_max_mean_rt_per_cc(temp_df)
+    if complete:
+        filename = filename + "complete_data.tsv"
         print(f"The complete df is built and it will be saved in {filename}")
-        final_complete_df.to_csv (path2res+"complete_treated_data.tsv", sep = "\t")
+        temp_df.to_csv (filename, sep = '\t', index = False)
         print(f"Complete saving the precessed file in {filename} !!")
         return
     else:
-        final_filtered_df = filter_by_n_mol(final_complete_df, down_threshold = down_threshold, up_threshold = up_threshold)
-        filename = path2res + "filtered_treated_data.tsv"
+        final_filtered_df = filter_by_n_mol(temp_df, down_threshold = down_threshold, up_threshold = up_threshold, apply_upthreshold = apply_upthreshold)
+        if apply_upthreshold:
+            filename = filename + "ds_data.tsv" #Note: ds stands for "downsampling"
+        else:
+            filename = filename + "no_ds_data.tsv"
         print(f"The filtered df is built and it will be saved in {filename}")
         final_filtered_df.to_csv(filename, sep = '\t', index = False)
         print(f"Complete saving the precessed file in {filename} !!")
         return
 
+if __name__ == "__main__":
+    get_processed_df_from_raw()

src/training/RepoRT/cc_split/perform_cc_splitting.py
@@ -17,20 +17,20 @@ from src.process_RepoRT_data.data_processing import get_processed_df_from_raw
 
 
 #DEFINE VARIABLES
-# input_path = "./data/processed_RepoRT/complete_treated_data.tsv" #Uncomment this line if want to use the complete data.
-input_path = "./data/processed_RepoRT/filtered_treated_data.tsv"
-output_dir = "./data/processed_RepoRT/cc_split_data/"
-complete = False # Set to True if want to use the data without filtering
+input_path = "./data/processed_RepoRT/with_SMRT_ds_data.tsv"
+output_dir = "./data/processed_RepoRT/with_SMRT_ds/cc_split_data/"
 
 # DEFINE THE FUNCTION
-def cc_split (input_path = input_path, output_dir = output_dir, complete=complete):
+def cc_split (input_path = input_path, output_dir = output_dir):
     print ("Checking the input file...")
     file = Path(input_path)
     if file.exists():
         print ("THe input file exists!")
     else:
         print ("The input file does not exist, creating it...")
-        get_processed_df_from_raw (complete=complete)
+        get_processed_df_from_raw (drop_smrt=False,
+                                   apply_upthreshold=True,
+                                   complete=False)
 
     print ("Getting the input DataFrame...")
     df = pd.read_csv (input_path, sep = "\t",)
@@ -40,9 +40,8 @@ def cc_split (input_path = input_path, output_dir = output_dir, complete=complet
 
     #Main process
     dir_ids = np.unique (df["dir_id"])
-    if not complete:
-        np.random.seed (42)
-        np.random.shuffle (dir_ids)
+    np.random.seed (42)
+    np.random.shuffle (dir_ids)
     print ("Splitting into differente sets")
     train_ids = []
     val_ids = []
@@ -74,7 +73,3 @@ def cc_split (input_path = input_path, output_dir = output_dir, complete=complet
 
 if __name__ == "__main__":
     cc_split ()
-    a = pd.read_csv (output_dir+"train_data.tsv", sep = "\t")
-    b = pd.read_csv (output_dir+"val_data.tsv", sep = "\t")
-    c = pd.read_csv (output_dir+"test_data.tsv", sep = "\t")
-    d = pd.read_csv (input_path, sep = "\t")

src/training/RepoRT/scaffold_split/main.py
@@ -6,6 +6,7 @@ Version: 1.1.
 Usage: Run the file to train and configure a MPNN using chemprop and processed data scaffold split from RepoRT.
 If the input datafiles do not exist, they will be built.
 Update: adapted to splitted_sets_functions.py version 1.1.
+Update (1.2.): adapted to splitted_sets_functions.py version 1.2. (Input data scaling, aka metadatada and gradient data)
 """
 
 
@@ -17,10 +18,10 @@ from src.training.functions.splitted_sets_functions import *
 from src.training.RepoRT.scaffold_split.perform_scaffold_split import ms_split
 # DEFINE PARAMETERS
 
-train_file = Path ("./data/processed_RepoRT/ms_split_data/train_data.tsv")
-test_file = Path ("./data/processed_RepoRT/ms_split_data/test_data.tsv")
-val_file = Path ("./data/processed_RepoRT/ms_split_data/val_data.tsv")
-path2res = "./logs/RepoRT/ms_split_res/Results_try_filtered/"
+train_file = Path ("./data/processed_RepoRT/with_SMRT_ds/ms_split_data/train_data.tsv")
+test_file = Path ("./data/processed_RepoRT/with_SMRT_ds/ms_split_data/test_data.tsv")
+val_file = Path ("./data/processed_RepoRT/with_SMRT_ds/ms_split_data/val_data.tsv")
+path2res = "./logs/RepoRT/with_SMRT_ds/ms_split_res/Results_try_filtered/"
 param_dict = {
     "mp_hidden_dim": 300,                             # Hidden dimension of the message passing (MP) part
     "mp_depth": 3,                                    # Depth/Number of Layers of the MP
@@ -36,6 +37,7 @@ param_dict = {
     "metric_list": [nn.MAE(), nn.RMSE()],
     "accelerator": "auto",                            # If GPU and CUDA available change to "gpu". Or can set "cpu" as well.
 }
+
 if __name__ == "__main__":
     if train_file.exists() and val_file.exists() and test_file.exists():
         print ("The input files are correct!")
@@ -48,6 +50,11 @@ if __name__ == "__main__":
 
     print ("Input data are successfully read. Making the output directory...")
     os.makedirs (path2res, exist_ok=True)
+
+    print ("Scaling the the metadata and gradient data using train Scaler...")
+    train_df, train_input_scaler = get_scaled_input_train_data(train_df)
+    val_df = get_scaled_datasets(val_df,train_input_scaler)
+    test_df = get_scaled_datasets(test_df,train_input_scaler)
 
     print ("Getting the DatLoaders...")
     train_loader, scaler, cc_shape = get_train_dataloader(train_df)

src/training/RepoRT/scaffold_split/perform_scaffold_split.py
@@ -18,20 +18,20 @@ from rdkit.Chem.Scaffolds import MurckoScaffold
 from src.process_RepoRT_data.data_processing import get_processed_df_from_raw
 
 #DEFINE VARIABLES
-# input_path = "./data/processed_RepoRT/complete_treated_data.tsv" #Uncomment this line if want to use the complete data.
-input_path = "./data/processed_RepoRT/filtered_treated_data.tsv"
-output_dir = "./data/processed_RepoRT/ms_split_data/"
-complete = False # Set to True if want to use the data without filtering
+input_path = "./data/processed_RepoRT/with_SMRT_ds_data.tsv"
+output_dir = "./data/processed_RepoRT/with_SMRT_ds/ms_split_data/"
 
 #DEFINE THE MAIN FUNCTION TO USE
-def ms_split (input_path=input_path, output_dir=output_dir, complete=complete):
+def ms_split (input_path=input_path, output_dir=output_dir):
     """
     Input: Path to processed DataFrame and Directory to save retult files.
     Outputs: Saves the train/val/test dsets in .tsv format inside the saving directory.
     """
     if not Path(input_path).exists():
         print ("The input file does not exist. Creating it...")
-        get_processed_df_from_raw (complete=complete)
+        get_processed_df_from_raw (drop_smrt=False,
+                                   apply_upthreshold=True,
+                                   complete=False)
 
     print ("Reading the input file...")
     df = pd.read_csv (input_path, sep='\t')
@@ -48,9 +48,8 @@ def ms_split (input_path=input_path, output_dir=output_dir, complete=complete):
 
     print (f"Getting train/val/test dsets and saving them in {output_dir}...")
     ms_smiles_classes = np.unique (final_df["ms_smiles"])
-    if not complete:
-        np.random.seed (42)
-        np.random.shuffle (ms_smiles_classes)
+    np.random.seed (42)
+    np.random.shuffle (ms_smiles_classes)
     train_dset, val_dset, test_dset = [], [], []
     train_size, val_size, test_size = 0, 0, 0
 
@@ -82,7 +81,3 @@ def ms_split (input_path=input_path, output_dir=output_dir, complete=complete):
 
 if __name__ == "__main__":
     ms_split ()
-    # a = pd.read_csv(output_dir + "train_data.tsv", sep="\t")
-    # b = pd.read_csv(output_dir + "val_data.tsv", sep="\t")
-    # c = pd.read_csv(output_dir + "test_data.tsv", sep="\t")
-    # d = pd.read_csv(input_path, sep="\t")

src/training/functions/splitted_sets_functions.py
@@ -2,25 +2,85 @@
 Name: splitted_sets_functions,py
 Author: Yixi Zhang
 Date: March 2026
-Version: 1.1.
+Version: 1.2.
 Description: contains the functions needed for training a MPNN (chemprop) with random split RepoRT data and write the result files.
 To do so, run ./src/training/RepoRT/random_split/main.py
-Update: A concept error was made in the previous version, as targets of each dataloader was scaled using a different Scaler. In this version, that has been fixed and a new function has been defined to
+Update (1.1.): A concept error was made in the previous version, as targets of each dataloader was scaled using a different Scaler. In this version, that has been fixed and a new function has been defined to
 get val/test_loader using the train_scaler of the train dataset.
 Updated metrics files, a new metric files will be created, it is a .tsv file containing the metrics (MAE, RMSE, %errors) for each repository contained in the test set.
 IMPORTANT: The %errors in the "metrics.txt" IS NOT THE MEAN VALUE OF THIS NEW FILE, they are calculate as mean of the metrics calculated from molecule to molecule.
+Update (1.2.): The functions to get the train scaler for the input (metadata and gradient data) and the scaled train set are defined here. The main difference in this new version
+is that the input files is no longer scaled but are scaled just before training.
+IMPORTANT: Here, the scaling data is not the whole dataset, but the metadata and gradient data for each repo are only considered 1 entree. In contrary, the input data
+(metadata and gradient data) would be conditioned to the molecule number from each cc, but if this were not the better way to do the scaling, it will be changed in future versions.
 """
 
 #IMPORT MODULES
 import numpy as np
 import pandas as pd
+
 from chemprop import data, nn, models, featurizers
 from rdkit.Chem.inchi import MolFromInchi
+from sklearn.preprocessing import StandardScaler
+
+import torch
 from lightning import pytorch as pl
 from lightning.pytorch.callbacks import EarlyStopping
-import torch
 
 #DEFINE FUNCTIONS
+def get_scaled_input_train_data (train_df):
+    """
+    Input: The train set used for training a model.
+    Output: the scaled train set and the Scaler
+    """
+
+    temp_list = []
+    train_input_scaler = StandardScaler ()
+    index_array = np.unique (train_df["dir_id"])
+    for index in index_array: #This loop is used to get the condition from each cc (chromatography condition)
+        temp_df = train_df[train_df["dir_id"] == index]
+        row = temp_df.iloc[:1]
+        temp_list.append (row)
+    temp_df = pd.concat(temp_list)
+    input_data = temp_df.loc[:, "column.length":]
+    train_input_scaler.fit (input_data) #This would be returned as output.
+    position = train_df.columns.get_loc("column.length")
+    columns_used = train_df.columns [position:]
+    train_df [columns_used] = train_input_scaler.transform(train_df [columns_used])
+    return train_df, train_input_scaler
+
+def get_scaled_datasets (df, train_input_scaler):
+    """
+    Used for scaling the input df using the train Scaler.
+    In this context, this is used to get test and val data using train_input_scaler.
+    """
+    position = df.columns.get_loc("column.length")
+    columns_used = df.columns[position:]
+    df [columns_used] = train_input_scaler.transform(df[columns_used])
+    return df
+
+def scaling_meta_grad_data (updated_df):
+    """
+    Requieres a df that only contains 1 metadata and gradient data per repo.
+    Returns the same dataframe but scaled.
+    """
+    temp_df = updated_df.loc[:, "column.length":].astype(float)
+    std_scaler = StandardScaler()
+    scaled_array = std_scaler.fit_transform(temp_df)
+    for column, array in zip (temp_df.columns, scaled_array.transpose()):
+        temp_df[column] = array
+    temp_df.insert(0, "dir_id", updated_df["dir_id"])
+    return temp_df
+
+def get_complete_scaled_df (df, updated_df):
+    """
+    Use the previous unscaled dataframe and the scaled metadata and gradient dataframe.
+    And merges them together to get the complete scaled dataframe.
+    """
+    temp_df = df.loc[:, :"column.usp.code_nan"]
+    final_df = pd.merge (temp_df, updated_df, on = "dir_id", how = "inner")
+    return final_df
+
 
 def get_train_dataloader (train_df):
     """